1da0ad72e2cef822803f18ec31c49bcb87e758d9
CameronD17 <camerondoyle@live.co.uk>
2015-12-08 14:46:08 +0000

    Changed blog layout to have a smaller header footprint, modified tag display

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,13 +90,13 @@ $('body').click(function() {
 // Toggle the 'Read more' and 'Read less' links (C) Cameron Doyle
 $('.read-more').click(function() {
     if ($(this).attr("aria-expanded") == "false")
-    {        
+    {
         $(this).find($(".fa")).removeClass('fa-toggle-down').addClass('fa-toggle-up');    // Switch the font-awesome up/down arrow
-        $(this).find($('span')).text($(this).text().replace('more','less'));            // Change text from 'more' to 'less'
+        $(this).find($('span')).text($(this).text().replace('more','less'));              // Change text from 'more' to 'less'
         $(this).find($('span')).text().trim();                                            // Trim trailing whitespace
     }
     else if($(this).attr("aria-expanded") == "true")
-    {    
+    {
         $(this).find($(".fa")).removeClass('fa-toggle-up').addClass('fa-toggle-down');
         $(this).find($('span')).text($(this).text().replace('less','more'));        
         $(this).find($('span')).text().trim();
---
+++ blog/tag/goals.markdown
@@ -0,0 +1,6 @@
+---
+layout:     blog_tag
+title:      Archive - Goals
+tag:        goals
+permalink:  /blog/tag/goals/
+---
