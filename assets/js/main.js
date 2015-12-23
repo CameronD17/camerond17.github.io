6582924af7992bc86b5f52c4235ea22dce113366
CameronD17 <camerondoyle@live.co.uk>
2015-12-23 14:38:49 +0000

    Split JS files to page-specifics

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,56 +35,6 @@ function openCollapsed() {
     }
 } window.onload = openCollapsed();
 
-// Avoid loading images on mobile (C) Cameron Doyle
-var loaded = false;
-
-function loadImages() {
-    if($(window).width() > 768 && !loaded)
-    {
-        loaded = true;
-        
-        var profile = document.getElementById("profile-img");
-        var profileImg=document.createElement("img");
-        profileImg.src="assets/img/profile.jpg"
-        profileImg.className="img-circle about-img";
-        profile.appendChild(profileImg);
-        
-        var blyth = document.getElementById("blyth-img");
-        var blythImg=document.createElement("img");
-        blythImg.src="assets/img/blyth.jpg"
-        blythImg.className="img-circle";
-        blyth.appendChild(blythImg);
-        
-        var code = document.getElementById("code-img");        
-        var codeImg=document.createElement("img");
-        codeImg.src="assets/img/code.jpg"
-        codeImg.className="img-circle";
-        code.appendChild(codeImg);
-        
-        var travel = document.getElementById("travel-img");
-        var travelImg=document.createElement("img");
-        travelImg.src="assets/img/travel.jpg"
-        travelImg.className="img-circle";
-        travel.appendChild(travelImg);
-        
-        var cgi = document.getElementById("cgi-img");
-        var cgiImg=document.createElement("img");
-        cgiImg.src="assets/img/cgi_logo.png"
-        cgiImg.className="img-circle";
-        cgi.appendChild(cgiImg);
-        
-        var graduation = document.getElementById("graduation-img");
-        var graduationImg=document.createElement("img");
-        graduationImg.src="assets/img/graduation.jpg"
-        graduationImg.className="img-circle";
-        graduation.appendChild(graduationImg);
-    }
-} window.onload = loadImages();
-
-$( window ).resize(function() {
-    loadImages();
-});
-
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
@@ -94,44 +44,5 @@ $('.navbar-collapse ul li a').click(function() {
 $('body').click(function() {
     if ($('.navbar-collapse').hasClass('collapse in')) {
         $('.navbar-collapse').collapse('hide');
-    }
-});
-
-// Toggle the 'Read more' and 'Read less' links (C) Cameron Doyle
-$('.read-more').click(function() {
-    if ($(this).attr("aria-expanded") == "false")
-    {
-        $(this).find($(".fa")).removeClass('fa-toggle-down').addClass('fa-toggle-up');    // Switch the font-awesome up/down arrow
-        $(this).find($('span')).text($(this).text().replace('more','less'));              // Change text from 'more' to 'less'
-        $(this).find($('span')).text().trim();                                            // Trim trailing whitespace
-    }
-    else if($(this).attr("aria-expanded") == "true")
-    {
-        $(this).find($(".fa")).removeClass('fa-toggle-up').addClass('fa-toggle-down');
-        $(this).find($('span')).text($(this).text().replace('less','more'));        
-        $(this).find($('span')).text().trim();
-    }
-});
-
-// Toggle skills on/off (C) Cameron Doyle
-$('.skill-toggle').click(function() {
-    
-    var level = $(this).attr('id');    
-    
-    if($(this).hasClass('selected'))
-    {
-        $(this).removeClass('selected');
-        $(this).find($(".fa")).removeClass('fa-check-square-o').addClass('fa-square-o');
-        $('.'+level).each(function() {                
-            $(this).addClass('hidden');
-        });
-    }
-    else
-    {
-        $(this).addClass('selected');            
-        $(this).find($(".fa")).removeClass('fa-square-o').addClass('fa-check-square-o');
-        $('.'+level).each(function() {
-            $(this).removeClass('hidden');
-        });
     }
 });
--- blog/_posts/2015-12-10-beginning.markdown
+++ blog/_posts/2015-12-10-beginning.markdown
@@ -1,11 +1,15 @@
 ---
 layout:     post
-title:      "In Development"
-date:       2015-12-01 10:03:00 +0000
+title:      "Beginning"
+date:       2015-12-10 10:00:00 +0000
 tags:       blog
 ---
-It's been a slow couple of weeks. I had originally anticipated the development of this blog to take one or two months, given that I've never tried it before, and I've never used any of the languages involved. But here we are, about 90% done before Christmas!
+It's been a slow couple of weeks at work. I had originally anticipated the development of this blog to take one or two months, maybe an hour or so every couple of nights, given that I've never tried it before, and I've never used any of the languages involved. But here we are, about 90% done before Christmas!
 
 <!-- Read More -->
 
-TODO
+The main reason for this is the rollercoaster approach I take towards development. Starting a project is a massive pain. Installing new development tools, unfamiliar commands and processes. Internal cursing and endless Google searches just to get a blank canvas, but a different canvas than that you've used before. Looking up online tutorials, biting off more than you can chew, why-doesn't-this-just-work, until suddenly it does. And it all clicks and it seems so easy.
+
+This happens to me every time. Learning Java, learning C++, learning Python, and now learning (the smallest smidge of) Ruby; it has happened every time. But once I start, once I have something I can get my teeth into, I can't stop. Not in the literal sense mind you, I can go get a coffee, I can take a step back and look at the bigger picture, but I can't just stop halfway. And when I do, it's like having a slight headache that you never really notice, except it will occasionally jab you in the skull and say "hey, what about that thing you didn't finish?".
+
+So, it's getting close. And I'm actually writing, rather than relying on test posts, which actually makes the layout look better (in the same way Lorem Ipsum looks better than repeating strings of letters at random I suppose). Hopefully, going forward, my writing will improve. But don't hold me to that.
