3ae20162ef33d7deaca5872f6f4d5872954f7b6d
CameronD17 <camerondoyle@live.co.uk>
2016-02-11 09:34:25 +0000

    Added blog filters by category

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -14,16 +14,16 @@ $('.read-more').click(function() {
     }
 });
 
-//sidebar scroll boundaries
+
+// Lock category pane to window (C) Cameron Doyle
 var list = $("#category-list");
 var listTop = $('#category-list').offset().top - 100;
-
   
 $(window).scroll(function() {    
     if (($(window).height() - 100) > $('#category-list').height())
     {
         var windowTop = $(window).scrollTop();    
-        var archiveTop = $('#blog-archive').offset().top + 50;
+        var archiveTop = $('#blog-archive').offset().top + 100;
         var archiveBottom = $('#blog-archive').offset().top + $('#blog-archive').height();
         var listBottom = listTop + $('#category-list').height(); 
 
@@ -42,5 +42,83 @@ $(window).scroll(function() {
             list.removeClass("category-fix-release");
             list.removeClass("category-list-fix");
         }
+    }
+});
+
+
+// Toggle blog categories on/off (C) Cameron Doyle
+$('.archive-category').click(function() {
+    
+    if($(this).hasClass('selected')) {
+        $(this).removeClass('selected');
+        $(this).find($(".fa")).removeClass('fa-check-square').addClass('fa-square');
+    }
+    else {
+        $(this).addClass('selected');            
+        $(this).find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
+    }    
+    
+    $('.archive-excerpt').each(function() {
+        if (!$(this).hasClass('hidden')) {
+            $(this).addClass('hidden');
+        }
+    });
+    
+    var none = true;
+    $('.archive-category').each(function() {
+        var category = $(this).attr('id');
+        if ($(this).hasClass('selected')) {
+            $('.archive-excerpt').each(function() {            
+                if ($(this).find('tr>td>span>a').hasClass(category)) {
+                    if ($(this).hasClass('hidden')) {
+                        $(this).removeClass('hidden');
+                        none = false;
+                    }
+                }
+            });
+        }
+    });
+    
+    if (none) {
+        list.removeClass("category-list-fix");
+    }
+});
+
+$('.archive-toggle').click(function() {
+
+    if($(this).hasClass('selectall'))
+    {
+        $('.archive-category').each(function() {
+            if (!$(this).hasClass('selected'))
+            {
+                $(this).addClass('selected');
+                $(this).find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
+            }
+        });
+        
+        $('.archive-excerpt').each(function() {
+            if ($(this).hasClass('hidden'))
+            {
+                $(this).removeClass('hidden');
+            }
+        });
+    }
+    else
+    {
+        list.removeClass("category-list-fix");
+        $('.archive-category').each(function() {
+            if ($(this).hasClass('selected'))
+            {
+                $(this).removeClass('selected');
+                $(this).find($(".fa")).removeClass('fa-check-square').addClass('fa-square');
+            }
+        });
+        
+        $('.archive-excerpt').each(function() {
+            if (!$(this).hasClass('hidden'))
+            {
+                $(this).addClass('hidden');
+            }
+        });
     }
 });
