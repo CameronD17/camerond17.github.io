14348c443ed0a0deba71d30fe8a50f0555b91da5
CameronD17 <camerondoyle@live.co.uk>
2016-02-12 14:10:32 +0000

    Fixed Issue #1, updated layout of Blog Archive

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,13 +1,11 @@
 // Toggle the 'Read more' and 'Read less' links (C) Cameron Doyle
 $('.read-more').click(function() {
-    if ($(this).attr("aria-expanded") == "false")
-    {
+    if ($(this).attr("aria-expanded") == "false") {
         $(this).find($(".fa")).removeClass('fa-toggle-down').addClass('fa-toggle-up');    // Switch the font-awesome up/down arrow
         $(this).find($('span')).text($(this).text().replace('more','less'));              // Change text from 'more' to 'less'
         $(this).find($('span')).text().trim();                                            // Trim trailing whitespace
     }
-    else if($(this).attr("aria-expanded") == "true")
-    {
+    else if($(this).attr("aria-expanded") == "true") {
         $(this).find($(".fa")).removeClass('fa-toggle-up').addClass('fa-toggle-down');
         $(this).find($('span')).text($(this).text().replace('less','more'));        
         $(this).find($('span')).text().trim();
@@ -15,45 +13,33 @@ $('.read-more').click(function() {
 });
 
 
-// Lock category pane to window (C) Cameron Doyle
-var list = $("#category-list");
-var listTop = $('#category-list').offset().top - 100;
-  
-$(window).scroll(function() {    
-    if (($(window).height() - 100) > $('#category-list').height())
-    {
-        var windowTop = $(window).scrollTop();    
-        var archiveTop = $('#blog-archive').offset().top + 100;
-        var archiveBottom = $('#blog-archive').offset().top + $('#blog-archive').height();
-        var listBottom = listTop + $('#category-list').height(); 
+// Lock category pane to window (C) Cameron Doyle  
+$(window).scroll(function() {
+    if ((($(window).height()) > $('#category-list').height()) && (($('#category-list').height() + 130) < $('#blog-archive').height())) {
+        var archiveTop          = $('#blog-archive').offset().top;
+        var archiveBottom       = archiveTop + $('#blog-archive').height();
+        var categoryListBottom  = $(window).scrollTop() + $('#category-list').height(); 
 
-        if (windowTop >= archiveTop)                    // Within Archive
-        {
-            list.removeClass("category-fix-release");
-            list.addClass("category-list-fix");
-        }
-        else if (listBottom >= archiveBottom)           // Below Archive
-        {
-            list.removeClass("category-list-fix");
-            list.addClass("category-fix-release");
-        }
-        else                                            // Above Archive
-        { 
-            list.removeClass("category-fix-release");
-            list.removeClass("category-list-fix");
+        if (($(window).scrollTop() >= archiveTop) && (categoryListBottom <= archiveBottom)) {   // Enter Archive
+            $("#category-list").removeClass("category-list-bottom");
+            $("#category-list").addClass("category-list-fixed");
+        } else if (categoryListBottom > archiveBottom){                                         // Exit Below Archive
+            $("#category-list").removeClass("category-list-fixed");
+            $("#category-list").addClass("category-list-bottom");
+        } else {                                                                                // Exit Above Archive
+            $("#category-list").removeClass("category-list-bottom");
+            $("#category-list").removeClass("category-list-fixed");
         }
     }
 });
 
 
 // Toggle blog categories on/off (C) Cameron Doyle
-$('.archive-category').click(function() {
-    
-    if($(this).hasClass('selected')) {
+$('.archive-category').click(function() {    
+    if ($(this).hasClass('selected')) {
         $(this).removeClass('selected');
         $(this).find($(".fa")).removeClass('fa-check-square').addClass('fa-square');
-    }
-    else {
+    } else {
         $(this).addClass('selected');            
         $(this).find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
     }    
@@ -80,45 +66,52 @@ $('.archive-category').click(function() {
     });
     
     if (none) {
-        list.removeClass("category-list-fix");
+        $("#category-list").removeClass("category-list-bottom");
+        $("#category-list").removeClass("category-list-fixed");
+        if($("#no-blog-posts").hasClass("hidden")) {
+            $("#no-blog-posts").removeClass("hidden");
+        }
+    } else if(!$("#no-blog-posts").hasClass("hidden")) {
+        $("#no-blog-posts").addClass("hidden");  
     }
 });
 
 $('.archive-toggle').click(function() {
-
-    if($(this).hasClass('selectall'))
-    {
+    if ($(this).hasClass('selectall')) {
         $('.archive-category').each(function() {
-            if (!$(this).hasClass('selected'))
-            {
+            if (!$(this).hasClass('selected')) {
                 $(this).addClass('selected');
                 $(this).find($(".fa")).removeClass('fa-square').addClass('fa-check-square');
             }
         });
         
         $('.archive-excerpt').each(function() {
-            if ($(this).hasClass('hidden'))
-            {
+            if ($(this).hasClass('hidden')) {
                 $(this).removeClass('hidden');
             }
         });
-    }
-    else
-    {
-        list.removeClass("category-list-fix");
+        
+        if(!$("#no-blog-posts").hasClass("hidden")) {
+            $("#no-blog-posts").addClass("hidden");  
+        }
+    } else {
+        $("#category-list").removeClass("category-list-bottom");
+        $("#category-list").removeClass("category-list-fixed");
         $('.archive-category').each(function() {
-            if ($(this).hasClass('selected'))
-            {
+            if ($(this).hasClass('selected')) {
                 $(this).removeClass('selected');
                 $(this).find($(".fa")).removeClass('fa-check-square').addClass('fa-square');
             }
         });
         
         $('.archive-excerpt').each(function() {
-            if (!$(this).hasClass('hidden'))
-            {
+            if (!$(this).hasClass('hidden')) {
                 $(this).addClass('hidden');
             }
         });
+        
+        if($("#no-blog-posts").hasClass("hidden")) {
+            $("#no-blog-posts").removeClass("hidden");
+        }
     }
 });
